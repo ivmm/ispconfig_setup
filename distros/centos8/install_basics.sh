6afc731ac889413c6b5aaa7c2b71b75ffc025c0b
妙正灰 <ivmmvobe@mf8.biz>
2023-03-30 17:05:56 +0800

    修改源

--- distros/centos8/install_basics.sh
+++ distros/centos8/install_basics.sh
@@ -9,7 +9,7 @@ InstallBasics() {
   echo -e "[${green}DONE${NC}]\n"
 
   echo -n "Installing basic packages... "
-  yum_install nano wget net-tools NetworkManager-tui selinux-policy deltarpm epel-release yum-priorities which
+  yum_install nano wget net-tools NetworkManager-tui selinux-policy deltarpm epel-release which
   rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY*
   echo -e "[${green}DONE${NC}]\n"
   
@@ -17,6 +17,11 @@ InstallBasics() {
   yum_install https://rpms.remirepo.net/enterprise/remi-release-8.rpm
   dnf module reset php
   dnf module disable php
+  echo -e "[${green}DONE${NC}]\n"
+
+  echo -n "Enable LLStack ISPConfig repository... "
+  sudo curl -o /etc/yum.repos.d/llstack-ISPconfig-Setup-centos-stream-8.repo https://copr.fedorainfracloud.org/coprs/llstack/ISPconfig-Setup/repo/centos-stream-8/llstack-ISPconfig-Setup-centos-stream-8.repo
+  yum config-manager --enable llstack-ISPconfig-Setup-centos-stream-8
   echo -e "[${green}DONE${NC}]\n"
 
   echo -n "Disabling Firewall... "
